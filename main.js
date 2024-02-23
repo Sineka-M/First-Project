060196504e70b9953294df4b833bd9c86975431a
Sineka-M <snehamahemdran@gmail.com>
2024-02-23 20:00:24 +0530

    basic javascript practice code

---
+++ main.js
@@ -0,0 +1,85 @@
+//var fruitname="Apple"
+//var count=5
+//var price=150
+//var total=0
+//total=count*price
+//console.log(total)
+//console.log(fruitname)
+//var a=2
+//var b=4
+//function add()
+//{
+//     console.log(a+b)
+// }
+// add()
+// var factor="kamal"
+// var fplayer="sachin"
+// var fmovie="Sillunuorukadhal"
+// function favourite()
+// {
+//     console.log(factor)
+//     console.log(fplayer)
+//     console.log(fmovie)
+// }
+//favourite()
+
+
+// function area(l,b)
+// {
+//     console.log(l*b)
+// }
+
+// area(10,20)
+
+// function add(a,b)
+// {
+//     return a+b
+//  }
+//  var total=add(10,20)
+//  console.log(total)
+
+//  add(10,20)
+
+
+// var homework=true
+// if(homework)
+// {
+//     console.log("Yes")
+// }
+// else{
+//     console.log("No")
+// }
+
+// var cookies=false
+// if(cookies)
+// {
+//     console.log("Would you like a cookie")
+// }
+// else{
+//     console.log("Time to bake more cookies")
+// }
+
+
+
+// var season= "spring"
+// if(season=="spring")
+// {
+// console.log("Enjoying the Blooming flowers")
+// }
+// if(season=="summer")
+// {
+//     console.log("Have fun in the sun")
+// }
+// if(season=="autumn")
+// {
+//     console.log("Admire the colorful")
+// }
+// if(season=="winter")
+// {
+//     console.log("Bundlr up and stay warm")
+// }
+
+
+
+
+    
